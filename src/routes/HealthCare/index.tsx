202c3c0fef32d03e511759852f1c42bf674e6fa0
DESKTOP-AG67U87\User <second3965@gmail.com>
2022-05-31 23:03:16 +0900

    Feat: add kb health calre main chart

--- src/routes/HealthCare/index.tsx
+++ src/routes/HealthCare/index.tsx
@@ -1,5 +1,16 @@
+import styles from './healthCare.module.scss'
+import Main from './Main'
+import Header from './Header/Header'
+
+import TotalChart from './Main/TotalChart'
+
 const HealthCare = () => {
-  return <div>HealthCare</div>
+  return (
+    <div className={styles.kbWrap}>
+      <Header />
+      <Main />
+    </div>
+  )
 }
 
 export default HealthCare
